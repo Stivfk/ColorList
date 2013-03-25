c8e35ba933fb3aeefdaff2f3f7c1261c386717a6
sfong <steeve.fong@gmail.com>
2013-03-25 14:29:14 +0400

    premier commit

---
+++ ColorList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorList
+//
+//  Created by Steeve Fong on 3/25/13.
+//  Copyright (c) 2013 Steeve Fong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
